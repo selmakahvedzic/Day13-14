6deb64be6c4cdf2c562494dd3dddebe2efa68552
Selma Kahvedžić <80279038+selmakahvedzic@users.noreply.github.com>
2021-03-21 22:40:08 +0100

    Update index.js

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,1 +1,14 @@
 
+
+function myFunction(imgs) {
+
+    var expandImg = document.getElementById("expandedImg");
+
+    var imgText = document.getElementById("imgtext");
+
+    expandImg.src = imgs.src;
+  
+    imgText.innerHTML = imgs.alt;
+
+    expandImg.parentElement.style.display = "block";
+  }
